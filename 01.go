1577e0114f46ab4560f9a5d7cd6f53d3bcc9a1d5
kmaasrud <km@aasrud.com>
2022-12-01 08:39:02 +0100

    day 1

---
+++ 01.go
@@ -0,0 +1,79 @@
+package main
+
+import (
+	"fmt"
+	"log"
+	"os"
+	"sort"
+	"strconv"
+	"strings"
+)
+
+func a(file string) int {
+    elves := strings.Split(file, "\n\n")
+
+    maxcals := 1
+    for _, foods := range elves {
+	foods := strings.Split(foods, "\n")
+
+	sum := 0
+	for _, food := range foods {
+	    cals, err := strconv.Atoi(food)
+
+	    if err != nil {
+		continue
+	    }
+
+	    sum += cals
+	}
+
+	if sum > maxcals {
+	    maxcals = sum
+	}
+    }
+
+    return maxcals
+}
+
+func b(file string) int {
+    elves := strings.Split(file, "\n\n")
+    var calsCarried []int
+    for _, foods := range elves {
+	foods := strings.Split(foods, "\n")
+
+	sum := 0
+	for _, food := range foods {
+	    cals, err := strconv.Atoi(food)
+
+	    if err != nil {
+		continue
+	    }
+
+	    sum += cals
+	}
+	calsCarried = append(calsCarried, sum)
+    }
+
+    sort.Ints(calsCarried)
+
+    sum := 0
+    for _, cals := range calsCarried[len(calsCarried)-3:] {
+	sum += cals
+    }
+
+    return sum
+}
+
+func main() {
+    content, err := os.ReadFile("data/01a.txt")
+
+    if err != nil {
+        log.Fatal(err)
+    }
+
+    maxcals := a(string(content))
+    fmt.Printf("The elf carrying the most energy, carries %d calories.\n", maxcals)
+
+    topThreeCals := b(string(content))
+    fmt.Printf("The three elves carrying the most energy, carry %d calories\n", topThreeCals)
+}
